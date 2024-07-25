2cbe8e350fc5907235f32f2ad18a8d254ee6c664
poorninr <116303124+poorninr@users.noreply.github.com>
2024-07-25 11:08:30 +0530

    Add files via upload

---
+++ Ecommerce/script.js
@@ -0,0 +1,12 @@
+var menubar=document.querySelector(".menubar");
+var menunavlink=document.querySelector(".menunavlink");
+function menuclick()
+{
+    menunavlink.style.left="0"
+}
+
+var menuxbutton=document.getElementById("menuxbutton");
+function menucancel()
+{
+    menunavlink.style.left="-60%"
+}
