e738ff111778571b92aa6b91abda3069dc875ab8
Poorni <poornimapnr5@gmail.com>
2024-08-16 15:03:45 +0530

    latest chages

--- Ecommerce/script.js
+++ Ecommerce/script.js
@@ -1,3 +1,4 @@
+// index page script
 var menubar=document.querySelector(".menubar");
 var menunavlink=document.querySelector(".menunavlink");
 function menuclick()
@@ -8,5 +9,24 @@ function menuclick()
 var menuxbutton=document.getElementById("menuxbutton");
 function menucancel()
 {
-    menunavlink.style.left="-60%"
+    menunavlink.style.left="-80%"
 }
+
+// collection page script
+var search=document.getElementById("input1")
+var collcontainer=document.querySelector(".collection-container")
+var element=collcontainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(){
+    var enteredText=event.target.value.toUpperCase()
+
+    for(i=0;i<element.length;i++){
+        var proname=element[i].querySelector("p").textContent
+        if(proname.toUpperCase().indexOf(enteredText)<0){
+            element[i].style.display="none"
+        }
+        else{
+            element[i].style.display="block"
+        }
+    }
+})
